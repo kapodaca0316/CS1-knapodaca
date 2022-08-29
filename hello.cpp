ce2f6fe88db4fd9f92ef115280fb5068df4549c7
knapodaca <kyraapodaca@gmail.com>
2022-08-29 11:35:26 -0600

    making comments

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,14 @@
+/*
+
+Name: Kyra Apodaca
+Date:8/29/2022
+
+This is a simple hellop world application. It takes no inputs and outputs "Hello World"
+*/
 #include <iostream>
 
 using namespace std;
 
 int main() {
-    cout << "Hello World" << endl;
+    cout << "Hello World" << endl; //prints Hello world to console
 }
