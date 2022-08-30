351a8ac76b6dbe02811ea5d19d7887859860a8da
knapodaca <kyraapodaca@gmail.com>
2022-08-30 11:15:59 -0600

    practice

---
+++ lectures/cpptypes.cpp
@@ -0,0 +1,19 @@
+/*
+Name: Kyra Apodaca
+
+This is demonstrating c++ data types
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    char myChar;
+
+    myChar = 'K';
+
+    cout <<myChar <<endl;
+    
+    return 0;
+}
