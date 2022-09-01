2da481740dac291333d75d4ac208a32e5fcea264
knapodaca <kyraapodaca@gmail.com>
2022-09-01 11:15:03 -0600

    myChar practice

---
+++ lectures/cpptypes2.cpp
@@ -0,0 +1,20 @@
+/* 
+Name: Kyra Apodaca
+Date: 09/01/2022
+
+This will demonstrate various types
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    char myChar;
+
+    myChar = 'N';
+
+    cout << "My char is: " << myChar << endl; 
+
+    return 0;
+}
