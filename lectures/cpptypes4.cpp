5ede5694c623c5119a8699d61025ae37f242c9d2
knapodaca <kyraapodaca@gmail.com>
2022-09-06 11:17:38 -0600

    starting rectangle calculator

---
+++ lectures/cpptypes4.cpp
@@ -0,0 +1,21 @@
+/*
+Name: Kyra Apodaca
+Date: 9/6/22
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+        string fullName;
+
+        cout << "Please enter your full name: ";
+        getline(cin, fullName);
+
+        cout << "Hello, " << fullName << endl; 
+
+return 0;
+}
---
+++ lectures/cpptypes5.cpp
@@ -0,0 +1,35 @@
+/* 
+Name: Kyra Apodaca
+Date: 9/6/22
+
+This program will calculate a rectangle
+
+Step 1: prompt for the users name
+Step 2: greet the user
+Step 7: prompt for sides of a rectangle 
+Step 8: calculate the area of a rectangle
+Step 9: calculate the perimeter of a rectangle
+Step 10: print the area and perimeter of a rectangle
+
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main () {
+    string firstName;
+    float side1, side2;
+    float area, perimeter; 
+
+    cout << "Welcome to my Rectangle Calculator!\n";
+    cout << "Please enter your first name: ";
+    getline(cin, firstName);
+
+    cout << "Hello " << firstName;
+
+
+
+return 0; 
+}
