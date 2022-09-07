0a7f1b99a22baa98576b070f029ef4426b1f8a8c
knapodaca <kyraapodaca@gmail.com>
2022-09-07 11:39:39 -0600

    squares

--- lectures/libraryex.cpp
+++ lectures/libraryex.cpp
@@ -5,10 +5,21 @@ Date: 09/07/2022
 
 #include <iostream>
 #include <string>
+#include <cstdlib>
+#include <cmath>
 
 using namespace std;
 
 int main () {
+//cout << pow(2, 2) << endl;
+    float num1;
+    int squared = 2;
+    
+    cout << "Please enter a number to square: ";
+    cin >> num1;
+
+    cout << num1 << "Squared is: "
+         << pow(num1, squared) << endl;
 
 return 0;
 }
