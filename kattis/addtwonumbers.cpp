e07a33980fc5b3166fe34661fa28f8f040e7f857
knapodaca <kyraapodaca@gmail.com>
2022-09-07 11:14:44 -0600

    kattis intro

---
+++ kattis/addtwonumbers.cpp
@@ -0,0 +1,22 @@
+/*
+Name: Kyra Apodaca
+
+Date: 09/07/2022
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    int num1, num2;
+    int total;
+
+    cin >> num1 >> num2;
+    total = num1 + num2;
+
+    cout << total << endl;
+    return 0;
+
+}
