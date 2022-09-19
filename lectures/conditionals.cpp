48b37ed0aa01be4903f71b94489af066184ba607
knapodaca <kyraapodaca@gmail.com>
2022-09-19 11:48:54 -0600

    conditionals

---
+++ lectures/conditionals.cpp
@@ -0,0 +1,66 @@
+/*
+Name: Kyra Apodaca
+
+Date: 9/19/2022
+*/
+
+#include <iostream>
+#include <string>
+#include <cassert>
+
+using namespace std;
+
+void readNumber();
+int addToNum(int);
+
+int main(int argc, char *argv[]) {
+    int num1, num2;
+
+    int readNumber();
+    num1 = readNumber();
+
+    if (num1 > 0) {
+        cout << "You entered a postive number" << endl;
+    } else if {
+        if (num1 < 0) {
+        cout << "You entered a negative number: " << endl;
+        } else if {
+        readNumber();
+        cout << "You entered 0" << endl;
+    }
+
+    // if (num1 < 0) {
+    //     cout << "You entered a negative number: " << endl;
+    // }
+
+    // if (num1 = 0) {
+    //     cout << "You've entered 0" << endl;
+    // }
+    
+    if (num1 == num2) {
+        cout << "The two numbers are equal" << endl;
+            if (num1 < num2) {
+                cout << "num1 is less than num2" << endl;
+            }
+    }
+
+    cout << "Outside of it" << endl;
+    //cout << boolalpha << (num1 == num2) << endl;
+    // cout << (num1 != num2) << endl;
+    // cout << (num1 < num2) << endl;
+    // cout << (num1 > num2) << endl;
+    // cout << (num1 >= num2) <<endl;
+    // cout << (num1 <= num2) << endl; 
+
+return 0;
+}
+
+void readNumber(int &bob) {
+    cout << "Please enter a whole number: ";
+    cin >> bob;
+}
+
+int addToNum(int a) {
+    a++;
+    return a;
+}
