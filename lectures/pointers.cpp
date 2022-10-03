ee224336717ce7fbb1ac28ea4c918a99a160595d
knapodaca <kyraapodaca@gmail.com>
2022-10-03 11:27:28 -0600

    pointer practice

--- lectures/pointers.cpp
+++ lectures/pointers.cpp
@@ -14,30 +14,72 @@ using namespace std;
 //void printName(string&);
 
 int main(int argc, char *argv[]) {
-    int side1;
-    int side2;
-    int *ptr;
-
-    side1 = 42;
-    side2 = 9000;
+    int num1;
+    int num2, num3, num4;
+    int *something;
     
-    ptr = &side1;
+    num1 = 42;
+    num2 = 15;
+    num3 = 9000;
+    num4 = -5;
+
+    cout << "&num1: " << &num1 << endl;
+    cout << "&num2: " << &num2 << endl;
+    cout << "&num3: " << &num3 << endl;
+    cout << "&num4: " << &num4 << endl;
+
+    something = &num1;
     
-    cout << "side1: " << side1 << endl;
-    cout << "ptr: " << *ptr << endl;
+    cout << *something << endl;
+    something++;
+    cout << *something << endl;
+     cout << *something << endl;
+    something++;
+     cout << *something << endl;
+    something++;
+    cout << *something << endl;
+    something--;
+    cout << *something << endl;
+     cout << *something << endl;
+    something--;
+     cout << *something << endl;
+    something--;
+    cout << *something << endl;
+    // cout << &num1 << endl;
+    // cout << &num2 << endl;
+    // cout << &num3 << endl;
+    // cout << &num4 << endl;
+    // cout << *something << endl;
 
-    side1 = 12;
+    // something = &num2;
 
-    cout << setw(20) << setfill(' ') << "" << endl;
-    cout << "side1: " << side1 << endl;
-    cout << "ptr: " << *ptr << endl;
+    // cout << *something << endl;
+    
+    
+    // int side1;
+    // int side2;
+    // int *ptr;
 
-    ptr = &side2;
-    *ptr = 100;
+    // side1 = 42;
+    // side2 = 9000;
+    
+    // ptr = &side1;
+    
+    // cout << "side1: " << side1 << endl;
+    // cout << "ptr: " << *ptr << endl;
 
-    cout << setw(20) << setfill(' ') << "" << endl;
-    cout << "side1: " << side1 << endl;
-    cout << "ptr: " << *ptr << endl;
+    // side1 = 12;
+
+    // cout << setw(20) << setfill(' ') << "" << endl;
+    // cout << "side1: " << side1 << endl;
+    // cout << "ptr: " << *ptr << endl;
+
+    // ptr = &side2;
+    // *ptr = 100;
+
+    // cout << setw(20) << setfill(' ') << "" << endl;
+    // cout << "side1: " << side1 << endl;
+    // cout << "ptr: " << *ptr << endl;
 
 
     // char middleInitial = 'K';
