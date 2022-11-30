9bbe9b84b1cebfdf0ea4d8d21f71a89432854fa5
knapodaca <kyraapodaca@gmail.com>
2022-11-29 19:31:15 -0700

    kattis homework

--- assignments/array/main.cpp
+++ assignments/array/main.cpp
@@ -26,14 +26,14 @@ void bubbleSort(int *, int);
 void whosWho(int *, int *, int *, int);
 large_int findSum(int *, int);
 void tests();
-void printArray(int nums[], int size);
 
 int main(int argc, char *argv[]) {
     size_t array, sizeA, sizeB;
 	
-    //cout << "array size:" << endl;
+    tests();
+
     cin >> array;
-    //cout << "array size:" << array << endl;
+
 	int *nums = new int[array];
 
     readData(nums, array);
@@ -46,28 +46,11 @@ int main(int argc, char *argv[]) {
     int *numsB = new int [sizeB];
 
     bubbleSort(nums, array);
-    //printArray(nums, array);
 
     whosWho(nums, numsA, numsB, array);
-
-    //cout << endl;
-    
-    cout << "Alice:";
-    printArray(numsA, sizeA);
-     cout << endl;
-
-    cout << "Bob:";
-    printArray(numsB, sizeB);
-     cout << endl;
      
     cout << findSum(numsA, sizeA) << endl;
     cout << findSum(numsB, sizeB) << endl;
-
-    //cout << "array size" << array << endl;
-    //cout << "size of numsA" << sizeA << endl;
-    //cout << "size of numsB" << sizeB << endl;
-
-    
 
     return 0;
 }
@@ -98,20 +81,19 @@ void bubbleSort (int nums[], int size) {
 void whosWho(int nums[], int numsA[], int numsB[], int size) {
     bool isA = true;
     int j = 0;
-    //cout << size;
-    //printArray(nums, size);
+    int k = 0;
+
     for (int i = size - 1; i >= 0; i--) {
-        cout << nums[i] << endl;
         if (isA) {
             numsA[j] = nums[i];
             isA = false;
-            cout << "A: " << numsA[j] << endl;
+            j++;
         } else {
-            numsB[j] = nums[i];
+            numsB[k] = nums[i];
             isA = true;
-            cout << "B: " << numsB[j] << endl;
+            k++;
         }
-        j++;
+        
     }
 }
 
@@ -125,12 +107,20 @@ large_int findSum(int nums[], int size) {
 }
 
 void tests() {
+    int test[5] = {9,7,8,10,13};
+    int test2[3] = {9,11,5};
+    int test3[7] = {11,7,12,19,2,6,7};
+    int resultA[3] = {5,3,1};
+    int resultB[2] = {4,2};
+    
+    int *numsA = new int [3];
+    int *numsB = new int [2];
 
-}
-
-void printArray(int nums[], int size) {
-	cout << "[ ";
-	for (int i = 0; i < size; i++)
-		cout << nums[i] << " ";
-	cout << "]" << endl;
+    assert(findSum(test, 5) == 47);
+    assert(findSum(test2, 3) == 25);
+    assert(findSum(test3, 7) == 64);
+    
+    whosWho(test, numsA, numsB, 5);
+    assert(numsA == resultA);
+    assert(numsB == resultB);
 }
