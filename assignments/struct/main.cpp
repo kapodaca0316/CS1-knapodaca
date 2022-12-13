df53b8d9746de7f1bd8833d48f06850129423513
knapodaca <kyraapodaca@gmail.com>
2022-12-13 14:28:44 -0700

    headerfiles

---
+++ assignments/struct/main.cpp
@@ -0,0 +1,19 @@
+/*
+Name: Kyra Apodaca
+Date: 12/12/2022
+
+1.Prompt the user for input file name to read the data from.
+2.Read an arbitrary number of student records into an array/vector.
+3.Prompt the user for the output file to write the output report to.
+
+4.Write function(s) to find students’ average test scores and the corresponding letter grade
+
+A (90% and above) 
+B (80% - 89%) 
+C (70% - 79%) 
+D (60% - 69%) 
+F (59% and less) 
+
+5.Write the values to the output text file using the tabular format as shown in the provided sample file called cs112.out.
+Write a function and use it to sort the array of students' records in descending order
+*/
